147ffaaa36d9f9f5ad78a207b9a04612ad3771df
norcs <norbertcs@me.com>
2018-01-11 09:38:31 +0100

    changed def name

--- gittie.py
+++ gittie.py
@@ -1,10 +1,8 @@
-def introduction():
+def introduce():
     return ("Hello gecc")
 
-<<<<<<< HEAD
 def joke():
     return("Why java developers has glasses. Because they can't C#")
-=======
+
 def add(a, b):
     return a + b
->>>>>>> 54c69ccc02f68591ab1740f624fda11b6ece0505
